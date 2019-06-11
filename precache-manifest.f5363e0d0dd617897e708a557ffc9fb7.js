2dc62a1f1804d7a43667e01fa0e6eac830a5d3bd
JoanLazo <joan162581@gmail.com>
2019-06-10 20:00:02 -0500

    Updates

---
+++ precache-manifest.f5363e0d0dd617897e708a557ffc9fb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18742521a04fd194b7991be719f3fe3",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "cd168fe923c4759e4529",
+    "url": "/star-wars/static/css/main.5bd380af.chunk.css"
+  },
+  {
+    "revision": "abdeb8fcb3943aabeef6",
+    "url": "/star-wars/static/js/2.bb5b5d65.chunk.js"
+  },
+  {
+    "revision": "cd168fe923c4759e4529",
+    "url": "/star-wars/static/js/main.e00303eb.chunk.js"
+  },
+  {
+    "revision": "aedf8bebf74d2f803586",
+    "url": "/star-wars/static/js/runtime~main.6cf174e3.js"
+  }
+]);
